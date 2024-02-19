bde62640a8cf3ad7c1282c3dd7104e1a005d9179
Ubuntu <ubuntu@ip-172-31-83-37.ec2.internal>
2024-02-19 23:50:27 +0000

    made changes to the files

---
+++ Eks-terraform/provider.tf
@@ -0,0 +1,14 @@
+# made changes
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
