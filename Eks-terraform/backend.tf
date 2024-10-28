1efe7b686246917fc72339a02b8dc6671031106a
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-10-28 19:32:15 +1100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kunalbarot-bucket" # Replace with your actual S3 bucket name
+    bucket = "tks-bucket-ttt-app" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
